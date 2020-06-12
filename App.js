0bd5200a5206f87e616eba02e79e38ce7ed32d3d
Farhan Asif <f.asif2010@gmail.com>
2020-06-12 23:25:05 +0600

    applying context

--- App.js
+++ App.js
@@ -3,19 +3,39 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as firebase from 'firebase';
+
+import AuthContextProvider from './src/contexts/AuthContext';
 
 import HomeComponent from './src/components/HomeComponent';
 import DetailsComponent from './src/components/DetailsComponent';
 
 const Stack = createStackNavigator();
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAyZ5nlER4Ft_246ppn0wJNppjoJo_7uBk",
+//   authDomain: "react-redux-c02aa.firebaseapp.com",
+//   databaseURL: "https://react-redux-c02aa.firebaseio.com",
+//   projectId: "react-redux-c02aa",
+//   storageBucket: "react-redux-c02aa.appspot.com",
+//   messagingSenderId: "662703363063",
+//   appId: "1:662703363063:web:8d98b7f0520d264c09d4f2",
+//   measurementId: "G-FKYVMN7L9M"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeComponent} />
-        <Stack.Screen name="Details" component={DetailsComponent} />
-      </Stack.Navigator>
+      <AuthContextProvider>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeComponent} />
+            <Stack.Screen name="Details" component={DetailsComponent} />
+        </Stack.Navigator>
+      </AuthContextProvider>
     </NavigationContainer>
   );
 }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,10 +1,20 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button, TouchableWithoutFeedback } from 'react-native';
+import { AuthContext } from '../contexts/AuthContext';
 
-export default function HomeScreen({ navigation }) {
+const HomeScreen = ({ navigation }) => {
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen here</Text>
+        <TouchableWithoutFeedback
+            onPress={() => toggleAuth()}
+        >
+            <View>
+                <Text style={{ padding: 20 }}>{ isAuthenticated ? 'Logged in' : 'Logged out' }</Text>
+            </View>
+        </TouchableWithoutFeedback>
         <Button
             title="Go to Details"
             onPress={() => navigation.navigate('Details')}
@@ -12,3 +22,5 @@ export default function HomeScreen({ navigation }) {
       </View>
     );
 }
+ 
+export default HomeScreen;
